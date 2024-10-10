7800162a2ec22cc649ea3a876636ab9c423e3c6e
Eznone <enzotresmediano@gmail.com>
2024-10-09 21:27:09 -0300

    Updated money method

--- BlackJack/src/Model/Deck.java
+++ BlackJack/src/Model/Deck.java
@@ -1,5 +1,55 @@
 package Model;
 
-public class Deck {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+class Deck {
+    private static Deck instance;
+    private List<String> cards;
+
+    // Private makes it so that no other deck can be made from outside
+    private Deck() {
+        cards = initializeDeck(8);
+    }
+
+    // Makes the singular possible deck through the use of static
+    public static Deck getInstance() {
+        if (instance == null) {
+            instance = new Deck();
+        }
+        return instance;
+    }
+
+    // Initialize the deck with 52 cards
+    private List<String> initializeDeck(int numDecks) {
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] values = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+        List<String> deck = new ArrayList<>();
+        
+        // Forloops to add cards for 8 total decks
+        for (int i = 0; i < numDecks; i++) {
+        	for (String suit : suits) {
+            	for (String value : values) {
+                	deck.add(value + " of " + suit);
+            	}
+        	}
+        }
+        
+        return deck;
+        
+    }
+
+    // Shuffle the deck
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    // Draw a card from the deck
+    public String drawCard() {
+        if (cards.isEmpty()) {
+            throw new IllegalStateException("No more cards in the deck!");
+        }
+        return cards.remove(cards.size() - 1);
+    }
 }
--- BlackJack/src/Model/Player.java
+++ BlackJack/src/Model/Player.java
@@ -1,5 +1,151 @@
 package Model;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
 
+// Player class
+class Player {
+    private String name;
+    private List<String> hand;
+    private int hundreds;
+    private int fifties;
+    private int twenties;
+    private int tens;
+    private int bet;
+
+    public Player(String name) {
+        this.name = name;
+        this.hand = new ArrayList<>();
+        this.hundreds = 10;
+        this.fifties = 10;
+        this.twenties = 20;
+        this.tens = 20;
+        this.bet = 0;
+    }
+
+    public void receiveCard(String card) {
+        hand.add(card);
+    }
+
+    public List<String> getHand() {
+        return hand;
+    }
+    
+    // Used to display total money player has
+    public int totalMoney() {
+    	int total = hundreds * 100;
+    	total += fifties * 50;
+    	total += twenties * 20;
+    	total += tens * 10;
+    	return total;
+    }
+    
+    // Method to track total bet
+    public int totalBet() {
+        return bet;
+    }
+    
+    // Method to place a bet
+    public boolean placeBet(int amount) {
+        int remainingAmount = amount;
+
+        // Deduct from hundreds
+        while (remainingAmount >= 100 && hundreds > 0) {
+            if (removeHundred()) {
+                remainingAmount -= 100;
+            }
+        }
+
+        // Deduct from fifties
+        while (remainingAmount >= 50 && fifties > 0) {
+            if (removeFifty()) {
+                remainingAmount -= 50;
+            }
+        }
+
+        // Deduct from twenties
+        while (remainingAmount >= 20 && removeTwenty()) {
+            remainingAmount -= 20;
+        }
+
+        // Deduct from tens
+        while (remainingAmount >= 10 && removeTen()) {
+            remainingAmount -= 10;
+        }
+
+        // If we were able to deduct the full amount, update the bet
+        if (remainingAmount == 0) {
+            bet += amount;  // Add the amount to the player's bet
+            return true;
+        }
+
+        // If unable to cover the full bet amount, restore denominations and return false
+        restoreFunds(amount - remainingAmount);
+        return false;
+    }
+    
+    // Method to double the player's current bet
+    public boolean doubleBet() {
+        int amountToDouble = bet;
+        // Check if player can cover the double
+        if (placeBet(amountToDouble)) {
+            bet += amountToDouble;  // Double the current bet
+            return true;
+        }
+        return false;  // Not enough funds to double the bet
+    }
+    
+    private void restoreFunds(int amount) {
+        while (amount >= 100) {
+            hundreds++;
+            amount -= 100;
+        }
+        while (amount >= 50) {
+            fifties++;
+            amount -= 50;
+        }
+        while (amount >= 20) {
+            twenties++;
+            amount -= 20;
+        }
+        while (amount >= 10) {
+            tens++;
+            amount -= 10;
+        }
+    }
+    
+    // Boolean becuase we want to say if player has token or not
+    // Display error of fault of token if returns false
+    public boolean removeHundred() {
+    	if (hundreds != 0) {
+    		hundreds -= 1;
+    		return true;
+    	}
+    	return false;
+    	
+    }
+    public boolean removeFifty() {
+    	if (fifties != 0) {
+    		fifties -= 1;
+    		return true;
+    	}
+    	return false;
+    	
+    }
+    public boolean removeTwenty() {
+    	if (twenties != 0) {
+    		twenties -= 1;
+    		return true;
+    	}
+    	return false;
+    	
+    }
+    public boolean removeTen() {
+    	if (tens != 0) {
+    		tens -= 1;
+    		return true;
+    	}
+    	return false;
+    	
+    }
 }
